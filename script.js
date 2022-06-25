beb2ebb57a048e8347dcb0cc553730785ee0127e
Kerdinand <ferdinandjonas.kloth@t-online.de>
2022-06-25 16:59:37 +0200

    dep-board

--- script.js
+++ script.js
@@ -43,20 +43,85 @@ const getLiveboard = async (station) => {
 	return obj;
 };
 
+const createTd = (content, mother) => {
+	let td = document.createElement('td');
+	td.innerHTML = content;
+	mother.appendChild(td);
+};
+
 const onMarkerClick = (e) => {
 	getLiveboard(e.target._popup._content).then((liveboard) => {
 		const content = document.createElement('div');
 		const header = document.createElement('h1');
-		const paragraph = document.createElement('p');
 		header.textContent = liveboard.station;
 		content.appendChild(header);
+		const table = document.createElement('table');
+		const tableContent = ['🕐', '🚅', '🚉', '🎯', 'CX'];
+		let tbody = document.createElement('tbody');
+		console.log(liveboard);
 
-		liveboard.departures.departure.forEach((train) => {
-			paragraph.textContent = train.vehicleinfo.shortnam;
-		});
-		content.appendChild(paragraph);
+		for (
+			let i = 0;
+			i < 40 && i < liveboard.departures.departure.length;
+			i++
+		) {
+			let th = document.createElement('th');
+			let delay = '';
+			if (i === 0) {
+				let trow = document.createElement('tr');
+				let trow2 = document.createElement('tr');
+				let thead = document.createElement('thead');
+				table.appendChild(thead);
+				thead.appendChild(trow);
+				tableContent.forEach((item) => {
+					let save = document.createElement('th');
+					save.innerHTML = item;
+					trow.appendChild(save);
+				});
+				table.appendChild(tbody);
+				tbody.appendChild(trow2);
+				let time = new Date(
+					liveboard.departures.departure[i].time * 1000
+				);
+				if (liveboard.departures.departure[i].delay != '0') {
+					delay = ' +' + liveboard.departures.departure[i].delay / 60;
+				}
+				createTd(time.toLocaleTimeString('it-IT') + delay, trow2);
+				createTd(
+					liveboard.departures.departure[i].vehicleinfo.shortname,
+					trow2
+				);
+				createTd(liveboard.departures.departure[i].platform, trow2);
+				createTd(liveboard.departures.departure[i].station, trow2);
+				createTd(liveboard.departures.departure[i].canceled, trow2);
+			} else {
+				let trow = document.createElement('tr');
+				trow.addEventListener('click', getTrainInfo);
+				let time = new Date(
+					liveboard.departures.departure[i].time * 1000
+				);
+
+				if (liveboard.departures.departure[i].delay != '0') {
+					delay = ' +' + liveboard.departures.departure[i].delay / 60;
+				}
+				createTd(time.toLocaleTimeString('it-IT') + delay, trow);
+				createTd(
+					liveboard.departures.departure[i].vehicleinfo.shortname,
+					trow
+				);
+				createTd(liveboard.departures.departure[i].platform, trow);
+				createTd(liveboard.departures.departure[i].station, trow);
+				createTd(liveboard.departures.departure[i].canceled, trow);
+				tbody.appendChild(trow);
+			}
+		}
+		content.appendChild(table);
 		timeTable.replaceChildren(content);
 	});
+};
+
+const getTrainInfo = (e) => {
+	console.log(e.originalTarget.parentNode.childNodes[1].textContent);
 };
 
 getBorders().then((coordinates) => {
